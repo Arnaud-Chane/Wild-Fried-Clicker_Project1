f815d5ec2d50c39a51d00ba893254a9f5bb5b032
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2023-03-23 11:29:49 +0100

    Delete resources directory

--- resources/js/main/colonelSong.js
+++
@@ -1,4 +0,0 @@
-function play() {
-  var audio = new Audio('resources/sounds/chickenTenders.mp3');
-  audio.play();
-}
--- resources/js/main/variables.js
+++
@@ -1,7 +0,0 @@
-let wings = 0;
-let wingsPerClick = 1;
-let wingsPerSecond = 0;
-let facebookFactory = 10;
-let twitterFactory = 100;
-let uberEatsFactory = 500;
-let quizzFactory = 1000;
