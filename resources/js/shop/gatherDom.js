fa7723de3e870aea59cff4bc64f9525a24d71dcd
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2023-03-23 22:53:16 +0100

    Delete resources directory

--- resources/js/shop/gatherDom.js
+++
@@ -1,8 +0,0 @@
-
-
-//DOM From Store
-let facebookStr = document.querySelector('#facebookStr');
-let twitterStr = document.querySelector('#twitterStr');
-let uberEatsStr = document.querySelector('#uberEatsStr');
-let quizzStr = document.querySelector('#quizzStr');
-
